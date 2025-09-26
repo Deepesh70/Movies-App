9ce097bd840a84fa4dfb865516cf495c94be140e
Deepesh70 <deepeshdangi700@gmail.com>
2025-09-26 21:46:34 +0530

    setup code of index file

--- server/index.js
+++ server/index.js
@@ -2,3 +2,42 @@ import express from 'express';
 import axios from 'axos';
 import dotenv from 'dotenv';
 import cors from cors;
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+//Search Route
+app.get('/api/search', async function(req, res){
+    try{
+        const { query } = req.query;  //get search query from request
+        if(!query){
+            return res.status(400).json({ message: 'Search query is required '});
+
+        }
+    const tmdbResponse = await axios.get(
+        'https://api.themoviedb.org/3/search/movie', {
+            params:{
+                api_key: process.env.TMDB_API_KEY,
+                query: query,
+            },
+        }
+    );
+
+// Send data from external api back to our react client
+    res.json(apiResponse.data);
+    } catch (errro){
+        console.error("Error fetching from TMDB API:" , error.message);
+        res.status(500).json({ message: "An error occured while fetching data,"});
+    }
+});
+
+app.listen(PORT, function(){
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
