b8d009ad5b776692a8e83ab57963597d12fb464e
Deepesh70 <deepeshdangi700@gmail.com>
2025-10-05 23:34:15 +0530

    new comments

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -61,7 +61,7 @@ function App() {
 
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
           {movies.map((movie) => (
-            <MovieCard key={movie.id} movie= {movie}/>
+            <MovieCard key={movie.id} movie= {movie}/>  // MovieCard component
           ))}
 
         </div>
